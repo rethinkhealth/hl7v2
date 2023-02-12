0941c304238af9d586b494f6e735ded7dc908559
Melek Somai <somaimelek@gmail.com>
2023-02-12 16:01:07 +0100

    Adding segment

---
+++ src/segment.ts
@@ -0,0 +1,83 @@
+import { SEQUENCE_STARTING_INDEX } from "./constants";
+import { DefaultDelimiters, IDelimiters } from "./delimiters";
+import { Element, IElement } from "./element";
+import { SegmentType } from "./enum";
+
+export interface ISegment {
+  delimiters: IDelimiters;
+  elements: IElement[];
+  name: SegmentType;
+  raw: string;
+  toJson(): any;
+}
+
+export interface SegmentOptions {
+  delimiters: IDelimiters;
+}
+
+export class Segment implements ISegment {
+  public readonly raw: string;
+
+  private _delimiters: IDelimiters;
+  public get delimiters(): IDelimiters {
+    return this._delimiters;
+  }
+
+  private _name: SegmentType;
+  public get name(): SegmentType {
+    return this._name;
+  }
+
+  private _elements: IElement[];
+  public get elements(): IElement[] {
+    return this._elements;
+  }
+
+  constructor(segment: string, private options?: SegmentOptions) {
+    this.raw = segment;
+    this._elements = [];
+    this._name = "" as any;
+    this._delimiters = {} as any;
+
+    // Configuration steps
+    this.setupDelimiters();
+    this.setupName();
+    this.setupElements();
+  }
+
+  public toJson() {
+    const response = {} as any;
+    this.elements.forEach((element) => {
+      response[element.sequence] = element.toJson();
+    });
+    return response;
+  }
+
+  private setupDelimiters() {
+    this._delimiters = this.options?.delimiters || DefaultDelimiters;
+  }
+
+  private setupName() {
+    const name = this.raw.split(this.delimiters.fieldSeparator)[0];
+    if (!Object.values(SegmentType).includes(name as unknown as SegmentType)) {
+      // TODO: Make it custom error handling for better management of errors.
+      throw new Error(
+        `Segment name not recognized. ${name} is not in the list.`
+      );
+    }
+    this._name = SegmentType[name as keyof typeof SegmentType];
+  }
+
+  private setupElements() {
+    const pieces = this.raw.split(this.delimiters.fieldSeparator);
+    // Create the elements by running through the pieces
+    pieces.forEach((value, index) => {
+      const sequenceNumber = index + SEQUENCE_STARTING_INDEX;
+      const sequence = `${this.name}.${sequenceNumber}`;
+      const element = new Element(value, sequence, {
+        delimiters: this._delimiters,
+      });
+      this._elements.push(element);
+    });
+  }
+}
---
+++ src/test/segment.test.ts
@@ -0,0 +1,84 @@
+import { Segment } from "../segment";
+
+describe.each([
+  ["AIG", "AIG|1||PULLEN^Pullen, Jeri|P^^CSI"],
+  [
+    "PID",
+    "PID|1||150||Bond^James^^007||19770920|M|||007 Soho Lane^^Cary^NC^27511||(919)007-0007^^^^^919^0070007~(777)707-0707^^CP^^^777^7070707~^NET^X.400^007@BritishSecretService.com|(919)851-6177 X007^^^^^919^8516177^007||M||150|007-00-0007|||||||||||N",
+  ],
+  [
+    "MSH",
+    "MSH|^~&|Ntierprise|Ntierprise Clinic|Healthmatics EHR|Healthmatics Clinic|20190423114154||SIU^S12|8907-45|P|2.3|||NE|NE",
+  ],
+])("HL7v2 Segment for %s", (expectedName, input) => {
+  it("should have the raw original segment", () => {
+    // When
+    const segment = new Segment(input);
+
+    // Then
+    expect(segment.raw).toEqual(input);
+  });
+
+  it("Name for the message should be %s", () => {
+    // When
+    const segment = new Segment(input);
+
+    // Then
+    expect(segment.name).toEqual(expectedName);
+  });
+
+  it("should raise an exception if the Segment Name is not identified", () => {
+    // Given
+    const incompatibleName = "XXR";
+    const modifiedInput = input.replace(expectedName, incompatibleName);
+
+    // When
+    expect(() => {
+      new Segment(modifiedInput);
+    }).toThrowError(
+      `Segment name not recognized. ${incompatibleName} is not in the list.`
+    );
+  });
+});
+
+describe("PID message", () => {
+  it("should return the elements with sequence for PID", () => {
+    // Given
+    const input =
+      "PID|1||150||Bond^James^^007||19770920|M|||007 Soho Lane^^Cary^NC^27511||(919)007-0007^^^^^919^0070007~(777)707-0707^^CP^^^777^7070707~^NET^X.400^007@BritishSecretService.com|(919)851-6177 X007^^^^^919^8516177^007||M||150|007-00-0007|||||||||||N";
+
+    // When
+    const segment = new Segment(input);
+
+    // Then
+    expect(segment.elements.length).toBe(31);
+    expect(segment.elements[0].sequence).toEqual("PID.1");
+    expect(segment.elements[0].value).toEqual("PID");
+  });
+
+  it("should return JSON format", () => {
+    // Given
+    const input =
+      "PID|1||150||Bond^James^^007||19770920|M|||007 Soho Lane^^Cary^NC^27511||(919)007-0007^^^^^919^0070007~(777)707-0707^^CP^^^777^7070707~^NET^X.400^007@BritishSecretService.com|(919)851-6177 X007^^^^^919^8516177^007||M||150|007-00-0007|||||||||||N";
+
+    // When
+    const segment = new Segment(input);
+
+    // Then
+    expect(segment.toJson()).toMatchSnapshot();
+  });
+});
+
+describe("MSH message", () => {
+  it("should return the elements with sequence for MSH", () => {
+    // Given
+    const input =
+      "MSH|^~&|Ntierprise|Ntierprise Clinic|Healthmatics EHR|Healthmatics Clinic|20190423114154||SIU^S12|8907-45|P|2.3|||NE|NE";
+
+    // When
+    const segment = new Segment(input);
+
+    // Then
+    expect(segment.elements.length).toBe(16);
+  });
+});
