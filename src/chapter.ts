b924f330ccba6cf6ff1e1352c11f47d6eaf07a8e
Melek Somai <somaimelek@gmail.com>
2023-04-05 11:58:54 +0100

    Adding more chapters

--- src/chapter.ts
+++ src/chapter.ts
@@ -6,7 +6,7 @@ export class Chapter {
   description: string;
   constructor(name: string) {
     // Check if the chapter exists
-    if (!Object.keys(Chapter).includes(name))
+    if (!Object.keys(chapters).includes(name))
       throw new Error(`Chapter ${name} not found`);
     // Retrieve the chapter
     const chapter = chapters[name as keyof typeof chapters];
@@ -15,5 +15,34 @@ export class Chapter {
     this.id = chapter.metadata.number;
     this.name = name;
     this.description = chapter.metadata.description;
+  }
+
+  public static findChapterById(id: number): Chapter | undefined {
+    const keys = Object.keys(chapters);
+    for (let i = 0; i < keys.length; i++) {
+      const chapter = chapters[keys[i] as keyof typeof chapters];
+      if (chapter.metadata.number === id) {
+        return new Chapter(keys[i]);
+      }
+    }
+  }
+
+  public static findChapterByEvent(message: string): Chapter | undefined {
+    const code = message.split("_")[0];
+    const event = message.split("_")[1];
+
+    const keys = Object.keys(chapters);
+    for (let i = 0; i < keys.length; i++) {
+      const chapter = chapters[keys[i] as keyof typeof chapters];
+      if (
+        chapter.events.find((e) =>
+          e.code.includes(code) && event !== ""
+            ? e.events.includes(event)
+            : true
+        )
+      ) {
+        return new Chapter(keys[i]);
+      }
+    }
   }
 }
--- src/test/chapter.test.ts
+++ src/test/chapter.test.ts
@@ -9,4 +9,43 @@ describe("Chapter", () => {
       `Chapter ${randomName} not found`
     );
   });
+
+  it("should return the correct chapter", () => {
+    // GIVEN
+    const chapterName = "Patient Administration";
+    // WHEN
+    const chapter = new Chapter(chapterName);
+    // THEN
+    expect(chapter.id).toBe(3);
+    expect(chapter.name).toBe(chapterName);
+    expect(chapter.description).toContain(
+      "The Patient Administration transaction set provides"
+    );
+  });
+
+  it("should return the correct chapter by id", () => {
+    // GIVEN
+    const chapterId = 4;
+    // WHEN
+    const chapter = Chapter.findChapterById(chapterId);
+    // THEN
+    expect(chapter).toBeDefined();
+    expect(chapter?.id).toBe(chapterId);
+    expect(chapter?.name).toBe("Order Entry");
+    expect(chapter?.description).toContain(
+      "The Order Entry transaction set provides for the transmission of orders"
+    );
+  });
+
+  it.each([
+    // ["ACK", "Patient Administration"],
+    ["ADT_A04", "Patient Administration"],
+  ])('should return the correct chapter by event "%s"', (message, expected) => {
+    // WHEN
+    const chapter = Chapter.findChapterByEvent(message);
+    // THEN
+    expect(chapter).toBeDefined();
+    expect(chapter?.id).toBe(3);
+    expect(chapter?.name).toBe(expected);
+  });
 });
