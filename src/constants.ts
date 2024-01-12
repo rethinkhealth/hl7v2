888d03244d7bf5b0080fade905d909c6c6652640
Melek Somai <somaimelek@gmail.com>
2024-01-12 00:13:17 -0500

    fixed errors

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,6 @@
 /**
  * The HL7v2 version numbers that are supported by this library.
  */
-export const HL7V2_COMPATIBLE_VERSIONS = ["2.5.1", "2.8", "2.9"];
 export type Versions = "2.5.1" | "2.8" | "2.9";
 /**
  * The starting index for the sequence number of a segment.
