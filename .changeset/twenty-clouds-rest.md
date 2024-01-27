109b4664bf16c41ba26a732a49287eccc921e809
Melek <1846037+meleksomai@users.noreply.github.com>
2024-01-27 01:00:29 -0500

    fix: Fixing nested groups without explicit segment (WIP) (#151)
    * fixing the bug with ORU
    
    * fixing group
    
    * fix ORU
    
    * fix

---
+++ .changeset/twenty-clouds-rest.md
@@ -0,0 +1,5 @@
+---
+"@rethinkhealth/hl7v2": patch
+---
+
+Resolved a bug when a group has nested groups without an explicit segment.
--- src/group.ts
+++ src/group.ts
@@ -117,59 +117,16 @@ export class Group extends Construct implements IGroup {
   private _assignGroup(index: number): number {
     const element = this._splits[index];
     const rootSegmentId = element.split(this.delimiters.fieldSeparator)[0];
-    const group = this.schema
+    const rootGroups = this.schema
       // step 1: retrieve the groups for the resource
-      ?.getGroups(this._options.resource)
-      // step 2: retrieve the segments for each group
-      .find((group) =>
-        Object.keys(
-          this.schema?.schema.$defs?.[group].properties || {},
-        ).includes(rootSegmentId),
-      );
+      ?.getGroups(this._options.resource);
+    // step 2: retrieve the segments for each group
+    const group = rootGroups?.find(
+      (group) => this.retrieveSegments(group)?.includes(rootSegmentId),
+    );
     if (group) {
-      let endIndex = index;
-      // find the end of the group. In this case, we have to ignore the
-      // first element because it is the group itself. We only want to
-      // find the end of the group. The end of the group is when we
-      // encounter a segment that is not part of the group  or its subgroups.
-      for (let i = index + 1; i < this._splits.length; i++) {
-        const elementId = this._splits[i].split(
-          this.delimiters.fieldSeparator,
-        )[0];
-        // if the element is the same as the root segment, then we should
-        // break since it means it is a new group.
-        if (elementId == rootSegmentId) {
-          break;
-        } else if (
-          // if the element is a segment of the group, then continue
-          Object.keys(
-            this.schema?.schema.$defs?.[group].properties || {},
-          ).includes(elementId) ||
-          // if the element starts with Z (custom segment), then it is a segment
-          // and we should continue
-          elementId.startsWith("Z") ||
-          // if the element is a segment of a subgroup, then continue
-          Object.keys(this.schema?.schema.$defs || {})
-            // get the subgroups associated with the group
-            .filter((a) =>
-              Object.keys(
-                this.schema?.schema.$defs?.[group].properties || {},
-              ).includes(a),
-            )
-            .some((a) =>
-              Object.keys(
-                this.schema?.schema.$defs?.[a].properties || {},
-              ).includes(elementId),
-            )
-        ) {
-          endIndex = i;
-        } else {
-          // if the element is not part of the group, then we have found the
-          // end of the group. Break out of the loop. We do not want to
-          // include the element that is not part of the group.
-          break;
-        }
-      }
+      const endIndex = this.findLastIndex(index, group, rootSegmentId);
+
       const subGroup = new Group(
         this,
         this._splits
@@ -189,8 +146,171 @@ export class Group extends Construct implements IGroup {
         this.groups[group] = subGroup;
       }
       return endIndex;
+    } else {
+      // This is an interesting use case where the segment is not part of the
+      // the group but instead could be part of the subgroups.
+      const parentGroupId = rootGroups?.find(
+        (group) => this.retrieveAllSegments(group)?.includes(rootSegmentId),
+      );
+
+      if (!parentGroupId) return index;
+
+      const parentGroup =
+        (this.groups[parentGroupId] as IGroup) ||
+        new Group(this, "", {
+          resource: parentGroupId,
+        });
+
+      const childGroupId = this.retrieveSubGroupBySegment(
+        parentGroupId,
+        rootSegmentId,
+      );
+
+      if (!childGroupId) return index;
+
+      const endIndex = this.findLastIndex(index, childGroupId, rootSegmentId);
+
+      const childGroup = new Group(
+        this,
+        this._splits
+          .slice(index, endIndex + 1)
+          .join(this.delimiters.terminator),
+        {
+          resource: childGroupId,
+        },
+      );
+
+      parentGroup.groups[childGroupId] = childGroup;
+
+      this.groups[parentGroupId] = parentGroup;
+
+      return endIndex;
     }
-    return index;
+  }
+
+  // ============================
+  // Protected methods
+  // ============================
+  protected retrieveAllSegments(group: string): string[] | undefined {
+    const segments = Object.keys(
+      this.schema?.schema.$defs?.[group].properties || {},
+    )
+      .flatMap((a) => {
+        // if the element is a segment of the group, return it
+        if (
+          // Check if it is a segment
+          this.schema?.schema.$defs?.[group].properties?.[a].$ref?.startsWith(
+            "segments.",
+          ) ||
+          // Check if it is anyOf of segements
+          this.schema?.schema.$defs?.[group].properties?.[a].anyOf?.some(
+            (b) => b.$ref?.startsWith("segments."),
+          )
+        ) {
+          return a;
+        } else {
+          // Loop through the subgroups and retrieve the segments of the subgroup
+          return this.retrieveAllSegments(a);
+        }
+      })
+      .filter((a) => a != undefined);
+
+    if (segments.length > 0) {
+      return segments as string[];
+    } else {
+      return undefined;
+    }
+  }
+
+  protected findLastIndex(index: number, group: string, rootSegmentId: string) {
+    let endIndex = index;
+    // find the end of the group. In this case, we have to ignore the
+    // first element because it is the group itself. We only want to
+    // find the end of the group. The end of the group is when we
+    // encounter a segment that is not part of the group  or its subgroups.
+    for (let i = index + 1; i < this._splits.length; i++) {
+      const elementId = this._splits[i].split(
+        this.delimiters.fieldSeparator,
+      )[0];
+      // if the element is the same as the root segment, then we should
+      // break since it means it is a new group.
+      if (elementId == rootSegmentId) {
+        break;
+      } else if (
+        // if the element is a segment of the group, then continue
+        Object.keys(
+          this.schema?.schema.$defs?.[group].properties || {},
+        ).includes(elementId) ||
+        // if the element starts with Z (custom segment), then it is a segment
+        // and we should continue
+        elementId.startsWith("Z") ||
+        // if the element is a segment of a subgroup, then continue
+        Object.keys(this.schema?.schema.$defs || {})
+          // get the subgroups associated with the group
+          .filter((a) =>
+            Object.keys(
+              this.schema?.schema.$defs?.[group].properties || {},
+            ).includes(a),
+          )
+          .some((a) =>
+            Object.keys(
+              this.schema?.schema.$defs?.[a].properties || {},
+            ).includes(elementId),
+          )
+      ) {
+        endIndex = i;
+      } else {
+        // if the element is not part of the group, then we have found the
+        // end of the group. Break out of the loop. We do not want to
+        // include the element that is not part of the group.
+        break;
+      }
+    }
+    return endIndex;
+  }
+
+  protected retrieveSubGroupBySegment(
+    group: string,
+    segementId: string,
+  ): string | undefined {
+    const subGroups = this.retrieveSubGroups(group);
+    if (subGroups) {
+      return subGroups.find(
+        (a) => this.retrieveSegments(a)?.includes(segementId),
+      );
+    }
+  }
+
+  protected retrieveSubGroups(group: string): string[] | undefined {
+    return Object.keys(
+      this.schema?.schema.$defs?.[group].properties || {},
+    ).filter(
+      (a) =>
+        // Check if it is a segment
+        this.schema?.schema.$defs?.[group].properties?.[a].$ref?.startsWith(
+          "/schemas/",
+        ) ||
+        // Check if it is anyOf of segements
+        this.schema?.schema.$defs?.[group].properties?.[a].anyOf?.some(
+          (b) => b.$ref?.startsWith("/schemas/"),
+        ),
+    );
+  }
+
+  protected retrieveSegments(group: string): string[] | undefined {
+    return Object.keys(
+      this.schema?.schema.$defs?.[group].properties || {},
+    ).filter(
+      (a) =>
+        // Check if it is a segment
+        this.schema?.schema.$defs?.[group].properties?.[a].$ref?.startsWith(
+          "segments.",
+        ) ||
+        // Check if it is anyOf of segements
+        this.schema?.schema.$defs?.[group].properties?.[a].anyOf?.some(
+          (b) => b.$ref?.startsWith("segments."),
+        ),
+    );
   }
 
   // ============================
