813d884c2d0715b1a39b6b41ca5d0dd77e50bbc6
Melek <1846037+meleksomai@users.noreply.github.com>
2025-07-26 13:06:00 -0400

    feat/monorepo (#220)
    * feat(create-turbo): apply official-starter transform
    
    * feat(create-turbo): apply pnpm-eslint transform
    
    * feat(create-turbo): install dependencies
    
    * chore: wip
    
    * chore: adding ultracite
    
    * adding support for linting and format
    
    * changeset
    
    ---------
    
    Co-authored-by: Turbobot <turbobot@vercel.com>

---
+++ .changeset/slow-cycles-brake.md
@@ -0,0 +1,5 @@
+---
+"@rethinkhealth/hl7v2": minor
+---
+
+Update to a monorepo package architectue
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-export { HL7v2Client } from "./client";
-export { DefaultDelimiters } from "./delimiters";
-export type { IDelimiters } from "./delimiters";
