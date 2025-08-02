bc92f778f4e29f487ef6aaf8d52088d97055fcef
Melek Somai <melek@rethinkhealth.io>
2025-08-02 14:33:39 -0400

    chore: fix

--- packages/hl7v2-parser/src/pipeline/index.ts
+++ packages/hl7v2-parser/src/pipeline/index.ts
@@ -1,21 +1,21 @@
 // Core pipeline components
 export { HL7MessageParser } from './core/message';
-export { IComponentParser as DefaultComponentParser } from './core/parsers/component';
-export { FieldParser as DefaultFieldParser } from './core/parsers/field';
+export { ComponentParser } from './core/parsers/component';
+export { FieldParser } from './core/parsers/field';
 // Built-in parsers
-export { SegmentParser as DefaultSegmentParser } from './core/parsers/segment';
+export { SegmentParser } from './core/parsers/segment';
 export { MSHSegmentParser } from './core/parsers/segment-msh';
-export { SubcomponentParser as DefaultSubcomponentParser } from './core/parsers/subcomponent';
+export { SubcomponentParser } from './core/parsers/subcomponent';
 export { SegmentParserRegistry } from './core/registry';
 // Interfaces
 export type {
   ComponentParseInput,
   FieldParseInput,
-  IComponentParser as ComponentParser,
-  IFieldParser as FieldParser,
-  IMessageParser as MessageParser,
-  ISegmentParser as SegmentParser,
-  ISubcomponentParser as SubcomponentParser,
+  IComponentParser,
+  IFieldParser,
+  IMessageParser,
+  ISegmentParser,
+  ISubcomponentParser,
   MessageParseInput,
   ParseContext,
   ParseInput,
