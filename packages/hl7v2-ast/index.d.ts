3e1399039ae48360a12155efe13abe7e88e509b2
Melek Somai <melek@rethinkhealth.io>
2025-08-02 09:04:20 -0400

    chore: update AST for HL7v2

--- packages/hl7v2-ast/index.d.ts
+++ packages/hl7v2-ast/index.d.ts
@@ -8,14 +8,16 @@ import type { Node, Position } from 'unist';
 export interface HL7v2Node extends Node {
   /**
    * The type of the node.
+   *
+   * There are 6 types of nodes:
+   * - **root**: The root node of the AST. This is the top-level node of the AST.
+   * - **segment**: A segment node (e.g. PID, MSH, etc.)
+   * - **header**: A header node (e.g. MSH, EVN, etc.)
+   * - **field**: A field node (e.g. PID.1, MSH.1, etc.)
+   * - **component**: A component node (e.g. PID.1.1, MSH.1.1, etc.)
+   * - **subcomponent**: A subcomponent node (e.g. PID.1.1.1, MSH.1.1.1, etc.)
    */
-  type:
-    | 'message'
-    | 'segment'
-    | 'header'
-    | 'field'
-    | 'component'
-    | 'subcomponent';
+  type: 'root' | 'segment' | 'header' | 'field' | 'component' | 'subcomponent';
 
   /**
    * The name of the node (e.g. "PID", "MSH", "EVN", etc.)
