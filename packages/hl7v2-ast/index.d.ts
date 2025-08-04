53905fb071d27562f9fdd94baa528c1ea6049fae
Melek Somai <melek@rethinkhealth.io>
2025-08-03 21:01:16 -0400

    feat: update AST tree structure.

--- packages/hl7v2-ast/index.d.ts
+++ packages/hl7v2-ast/index.d.ts
@@ -1,24 +1,76 @@
-import type { Node, Position } from 'unist';
+import type {
+  Data as UnistData,
+  Literal as UnistLiteral,
+  Node as UnistNode,
+  Parent as UnistParent,
+  Position as UnistPosition,
+} from 'unist';
+
+// ## Abstract nodes
 
 /**
- * HL7v2 Node specification following the Unist spec
+ * Union of registered HL7v2 literals.
+ *
+ * To register custom HL7v2 nodes, add them to {@link RootContentMap} and other
+ * places where relevant.
+ * They will be automatically added here.
+ */
+export type Literals = Extract<Nodes, UnistLiteral>;
+
+/**
+ * Union of registered HL7v2 parents.
+ *
+ * To register custom HL7v2 nodes, add them to {@link RootContentMap} and other
+ * places where relevant.
+ * They will be automatically added here.
+ */
+export type Parents = Extract<Nodes, UnistParent>;
+
+/**
+ * Union of registered HL7v2 nodes.
+ *
+ * To register custom HL7v2 nodes, add them to {@link RootContentMap} and other
+ * places where relevant.
+ * They will be automatically added here.
+ */
+export type Nodes = Root | RootContent;
+
+/**
+ * Info associated with HL7v2Node nodes by the ecosystem.
+ *
+ * This space is guaranteed to never be specified by unist or HL7v2Node.
+ * But you can use it in utilities and plugins to store data.
+ *
+ * This type can be augmented to register custom data.
+ * For example:
+ *
+ * ```ts
+ * declare module '@rethinkhealth/hl7v2-ast' {
+ *   interface Data {
+ *     // `someNode.data.myId` is typed as `number | undefined`
+ *     myId?: number | undefined
+ *   }
+ * }
+ * ```
+ */
+export interface Data extends UnistData {}
+
+/**
+ * Abstract HL7v2 node.
+ *
+ * This interface is supposed to be extended.
+ * If you can use {@link Literal} or {@link Parent}, you should.
+ * But for example in HL7v2, a `Delimiter` is neither literal nor parent, but
+ * still a node.
+ *
+ * To register custom HL7v2 nodes, add them to {@link RootContentMap} and other
+ * places where relevant (such as {@link ElementContentMap}).
+ *
+ * For a union of all registered HL7v2 nodes, see {@link Nodes}.
  *
  * @see https://github.com/syntax-tree/unist#node
  */
-export interface HL7v2Node extends Node {
-  /**
-   * The type of the node.
-   *
-   * There are 6 types of nodes:
-   * - **root**: The root node of the AST. This is the top-level node of the AST.
-   * - **segment**: A segment node (e.g. PID, MSH, etc.)
-   * - **header**: A header node (e.g. MSH, EVN, etc.)
-   * - **field**: A field node (e.g. PID.1, MSH.1, etc.)
-   * - **component**: A component node (e.g. PID.1.1, MSH.1.1, etc.)
-   * - **subcomponent**: A subcomponent node (e.g. PID.1.1.1, MSH.1.1.1, etc.)
-   */
-  type: 'root' | 'segment' | 'header' | 'field' | 'component' | 'subcomponent';
-
+export interface Node extends UnistNode {
   /**
    * The name of the node (e.g. "PID", "MSH", "EVN", etc.)
    */
@@ -33,27 +85,241 @@ export interface HL7v2Node extends Node {
   index?: number;
 
   /**
-   * The value of the node. This is the raw value of the node.
-   *
-   * TODO: We might consider removing this property and use the children
-   * property instead.
-   */
-  value?: string;
-
-  /**
-   * The delimiter of the node.
-   */
-  delimiter?: string;
-
-  /**
-   * The children of the node.
-   */
-  children?: HL7v2Node[];
-
-  /**
    * The position of the node in the source text.
    *
    * This is used for round-tripping the AST back to the original text.
    */
-  position?: Position;
+  position?: UnistPosition;
+
+  /**
+   * Info from the ecosystem.
+   */
+  data?: Data | undefined;
 }
+
+/**
+ * Abstract HL7v2 node that contains the smallest possible value.
+ *
+ * This interface is supposed to be extended if you make custom HL7v2 nodes.
+ *
+ * For a union of all registered mdast literals, see {@link Literals}.
+ */
+export interface Literal extends Node {
+  /**
+   * Plain-text value.
+   */
+  value: string;
+}
+
+/**
+ * Abstract HL7v2 node that contains other HL7v2 nodes (*children*).
+ *
+ * This interface is supposed to be extended if you make custom HL7v2 nodes.
+ *
+ * For a union of all registered mdast parents, see {@link Parents}.
+ */
+export interface Parent extends Node {
+  /**
+   * List of children.
+   */
+  children: RootContent[];
+}
+
+// ## Content Maps
+
+/**
+ * Union of registered mdast nodes that can occur in {@link Root}.
+ *
+ * To register custom mdast nodes, add them to {@link RootContentMap}.
+ * They will be automatically added here.
+ */
+export type RootContent = RootContentMap[keyof RootContentMap];
+
+/**
+ * Registry of all mdast nodes that can occur as children of {@link Root}.
+ *
+ * > **Note**: {@link Root} does not need to be an entire document.
+ * > it can also be a fragment.
+ *
+ * This interface can be augmented to register custom node types:
+ *
+ * ```ts
+ * declare module 'mdast' {
+ *   interface RootContentMap {
+ *     // Allow using toml nodes defined by `remark-frontmatter`.
+ *     toml: TOML;
+ *   }
+ * }
+ * ```
+ *
+ * For a union of all {@link Root} children, see {@link RootContent}.
+ */
+export interface RootContentMap {
+  segment: Segment;
+  group: Group;
+  field: Field;
+  fieldRepetition: FieldRepetition;
+  component: Component;
+  subcomponent: Subcomponent;
+}
+
+// ## Concrete nodes
+
+/**
+ * Document fragment or a whole document.
+ *
+ * Should be used as the root of a tree and must not be used as a child.
+ */
+export interface Root extends Parent {
+  /**
+   * Node type of HL7v2 root.
+   */
+  type: 'root';
+  /**
+   * Data associated with the mdast root.
+   */
+  data?: RootData | undefined;
+}
+
+/**
+ * Info associated with HL7v2 root nodes by the ecosystem.
+ */
+export interface RootData extends Data {}
+
+/**
+ * HL7v2 segment.
+ */
+export interface Segment extends Parent {
+  /**
+   * Node type of HL7v2 segment.
+   */
+  type: 'segment';
+  /**
+   * Children of block quote.
+   */
+  children: Field[];
+  /**
+   * Data associated with the mdast block quote.
+   */
+  data?: SegmentData | undefined;
+}
+
+/**
+ * Info associated with HL7v2 segment nodes by the ecosystem.
+ */
+export interface SegmentData extends Data {}
+
+/**
+ * HL7v2 group.
+ *
+ * A group is a set of segments that appear together in a defined order within a message.
+ * They are often used when a message needs to repeat a set of related segments together
+ * rather than just a single segment.
+ *
+ * Groups can be required/optional and repeating/non-repeating, just like segments.
+ */
+export interface Group extends Parent {
+  /**
+   * Node type of HL7v2 segment.
+   */
+  type: 'group';
+  /**
+   * Children of block quote.
+   */
+  children: Segment[];
+  /**
+   * Data associated with the mdast block quote.
+   */
+  data?: GroupData | undefined;
+}
+
+/**
+ * Info associated with HL7v2 segment nodes by the ecosystem.
+ */
+export interface GroupData extends Data {}
+
+/**
+ * HL7v2 field.
+ */
+export interface Field extends Parent {
+  /**
+   * Node type of HL7v2 field.
+   */
+  type: 'field';
+  /**
+   * Children of field.
+   */
+  children: FieldRepetition[];
+  /**
+   * Data associated with the mdast block quote.
+   */
+  data?: FieldData | undefined;
+}
+
+/**
+ * Info associated with HL7v2 segment nodes by the ecosystem.
+ */
+export interface FieldData extends Data {}
+
+/**
+ * HL7v2 field repetition.
+ */
+export interface FieldRepetition extends Parent {
+  /**
+   * Node type of HL7v2 field repetition.
+   */
+  type: 'field-repetition';
+  /**
+   * Children of field repetition.
+   */
+  children: Component[];
+  /**
+   * Data associated with the mdast block quote.
+   */
+  data?: FieldRepetitionData | undefined;
+}
+
+/**
+ * Info associated with HL7v2 field repetition nodes by the ecosystem.
+ */
+export interface FieldRepetitionData extends Data {}
+
+/**
+ * HL7v2 component.
+ */
+export interface Component extends Parent {
+  /**
+   * Node type of HL7v2 component.
+   */
+  type: 'component';
+  /**
+   * Children of component.
+   */
+  children: Subcomponent[];
+  /**
+   * Data associated with the mdast block quote.
+   */
+  data?: ComponentData | undefined;
+}
+
+/**
+ * Info associated with HL7v2 segment nodes by the ecosystem.
+ */
+export interface ComponentData extends Data {}
+
+/**
+ * HL7v2 subcomponent.
+ */
+export interface Subcomponent extends Literal {
+  /**
+   * Node type of HL7v2 subcomponent.
+   */
+  type: 'subcomponent';
+
+  /**
+   * Data associated with the mdast block quote.
+   */
+  data?: SubcomponentData | undefined;
+}
+
+export interface SubcomponentData extends Data {}
