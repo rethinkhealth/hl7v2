4ac0f31ee9dc6dabde61b6ecc8d6543b2c08d903
Melek Somai <melek@rethinkhealth.io>
2025-08-17 00:59:22 -0400

    feat: add a new recommended linting preset for HL7v2

---
+++ packages/hl7v2-preset-lint-recommended/src/index.ts
@@ -0,0 +1,33 @@
+import hl7v2LintSegmentHeaderLength from '@rethinkhealth/hl7v2-lint-segment-header-length';
+import type { Preset } from 'unified';
+
+/**
+ * Preset of hl7v2-lint rules to warn for likely problems.
+ *
+ * ## What is this?
+ *
+ * This package is a preset containing `hl7v2-lint` rules.
+ * Lint rules check hl7v2 basic rules.
+ *
+ * ## When should I use this?
+ *
+ * You can use this package to check that hl7v2 follows some best practices.
+ *
+ * ## API
+ *
+ * ### `unified().use(hl7v2PresetLintRecommended)`
+ *
+ * Check that hl7v2 follows some best practices.
+ *
+ * You can reconfigure rules in the preset by using them afterwards with different
+ * options.
+ *
+ */
+const hl7v2PresetLintRecommended: Preset = {
+  plugins: [
+    // segment linting rules
+    [hl7v2LintSegmentHeaderLength, ['error']],
+  ],
+};
+
+export default hl7v2PresetLintRecommended;
---
+++ packages/hl7v2-preset-lint-recommended/tests/index.test.ts
@@ -0,0 +1,30 @@
+import { parseHL7v2 } from '@rethinkhealth/hl7v2';
+import { describe, expect, it } from 'vitest';
+import hl7v2PresetLintRecommended from '../src/index';
+
+describe('HL7v2 lint preset', () => {
+  it('errors when segment header length is invalid', async () => {
+    // GIVEN
+    const msg = [
+      // MSH
+      'MSH|^~\\&|SENDER|FAC|RCVR|FAC|20250101010101||ADT^A01|MSG00001|P|2.5',
+      // PID with invalid header length
+      'PIDTOOLONG|||PATID1234^5^M11^ADT1^MR^UNIVERSITY HOSPITAL~123_456_789^^^USSSA^SS||EVERYMAN^ADAM^A^III||19_610_615|M||C|1200 N ELM STREET^^GREENSBORO^NC^27_401-1020|GL|(919)379-1212|(919)271-3434||S||PATID12345001^2^M10^ADT1^AN^A|123_456_789|9-87_654^NC',
+      // OBX
+      'OBR|1|845439^GHH OE|1045813^GHH LAB|1554-5^GLUCOSE^LN|||200202150730|||||||||DOCT^KILDARE^JAMES^A|||||||200202150930||F|||^^^^^R',
+    ].join('\r');
+
+    // WHEN
+    const file = await parseHL7v2()
+      .use(hl7v2PresetLintRecommended)
+      .process(msg);
+
+    // THEN
+    expect(file.messages).toHaveLength(1);
+    expect(file.messages[0].message).toBe(
+      'Unexpected 10 header length, expected 3 characters, remove 7 characters'
+    );
+
+    expect(file.messages[0].fatal).toBe(true);
+  });
+});
