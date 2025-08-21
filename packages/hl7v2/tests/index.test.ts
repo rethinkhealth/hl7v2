e879a56b221f66ed9388c29c46ffdd6a91f4f2bb
Melek Somai <melek@rethinkhealth.io>
2025-08-21 23:04:43 +0300

    chore: minor fixes

--- packages/hl7v2/tests/index.test.ts
+++ packages/hl7v2/tests/index.test.ts
@@ -1,3 +1,4 @@
+import { DEFAULT_DELIMITERS } from '@rethinkhealth/hl7v2-utils';
 import { describe, expect, it } from 'vitest';
 import { parseHL7v2 } from '../src';
 
@@ -20,6 +21,19 @@ describe('parseHL7v2 (holistic)', () => {
     const file = await parseHL7v2.process(msg);
 
     expect(file.value).toMatchSnapshot();
+  });
+
+  it('includes delimiters in the output', () => {
+    const msg = [
+      'MSH|^~\\&|SENDER|FAC|RCVR|FAC|20250101010101||ADT^A01|MSG00001|P|2.5',
+      'PID|||PATID1234^5^M11^ADT1^MR^UNIVERSITY HOSPITAL~123_456_789^^^USSSA^SS||EVERYMAN^ADAM^A^III||19_610_615|M||C|1200 N ELM STREET^^GREENSBORO^NC^27_401-1020|GL|(919)379-1212|(919)271-3434||S||PATID12345001^2^M10^ADT1^AN^A|123_456_789|9-87_654^NC',
+      'OBR|1|845439^GHH OE|1045813^GHH LAB|1554-5^GLUCOSE^LN|||200202150730|||||||||DOCT^KILDARE^JAMES^A|||||||200202150930||F|||^^^^^R',
+    ].join('\r');
+
+    const root = parseHL7v2.parse(msg);
+
+    expect(root.data?.delimiters).toBeDefined();
+    expect(root.data?.delimiters).toEqual(DEFAULT_DELIMITERS);
   });
 
   it('handles decoding escapes', async () => {
