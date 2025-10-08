4b4cd871dbd518a5bd7962fcc3b5ca0a798c9e91
Melek Somai <melek@rethinkhealth.io>
2025-10-07 23:16:00 -0400

    chore: updated the package header lint

--- packages/hl7v2-lint-segment-header-length/src/index.ts
+++ packages/hl7v2-lint-segment-header-length/src/index.ts
@@ -1,46 +1,40 @@
-import type { Node, Segment } from "@rethinkhealth/hl7v2-ast";
+import type { Root } from "@rethinkhealth/hl7v2-ast";
 import pluralize from "pluralize";
 import { lintRule } from "unified-lint-rule";
-import { CONTINUE, SKIP, visitParents } from "unist-util-visit-parents";
+import { SKIP, visitParents } from "unist-util-visit-parents";
 
 /**
  * hl7v2-lint rule to warn when segment header length is invalid.
  *
  *
  */
-const hl7v2LintSegmentHeaderLength = lintRule<Node, undefined>(
+const hl7v2LintSegmentHeaderLength = lintRule<Root, undefined>(
   {
     origin: "hl7v2-lint:segment-header-length",
     url: "https://github.com/rethinkhealth/hl7v2/tree/main/packages/hl7v2-lint-segment-header-length#readme",
   },
   (tree, file) => {
-    visitParents(tree, (node, parents) => {
-      if (node.type === "root") {
-        return CONTINUE;
+    visitParents<Root, "segment-header">(
+      tree,
+      "segment-header",
+      (node, parents) => {
+        const headerValue = node.value;
+        const size = headerValue?.length ?? 0;
+
+        if (size === 3) {
+          return SKIP;
+        }
+
+        file.message(
+          `Unexpected ${size} header length, expected 3 characters, ${
+            size > 3
+              ? `remove ${size - 3} ${pluralize("character", size - 3)}`
+              : `add ${3 - (size ?? 0)} ${pluralize("character", 3 - (size ?? 0))}`
+          }`,
+          { ancestors: [...parents, node], place: node.position }
+        );
       }
-
-      if (node.type !== "segment") {
-        return SKIP;
-      }
-
-      const segment = node as Segment;
-      // Prefer the name property if present, otherwise extract from first field
-      const headerValue = segment.name;
-      const size = headerValue?.length ?? 0;
-
-      if (size === 3) {
-        return SKIP;
-      }
-
-      file.message(
-        `Unexpected ${size} header length, expected 3 characters, ${
-          size > 3
-            ? `remove ${size - 3} ${pluralize("character", size - 3)}`
-            : `add ${3 - (size ?? 0)} ${pluralize("character", 3 - (size ?? 0))}`
-        }`,
-        { ancestors: [...parents, node], place: node.position }
-      );
-    });
+    );
   }
 );
 
