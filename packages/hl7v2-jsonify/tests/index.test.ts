e60f1d2b3306eb456901d032fdc94dcce5ca5319
Melek Somai <melek@rethinkhealth.io>
2025-10-31 18:37:50 -0400

    update jsonify unit tests

--- packages/hl7v2-jsonify/tests/index.test.ts
+++ packages/hl7v2-jsonify/tests/index.test.ts
@@ -1,19 +1,32 @@
-import { hl7v2Parser } from "@rethinkhealth/hl7v2-parser";
+import { f, m, s } from "@rethinkhealth/hl7v2-builder";
 import { unified } from "unified";
 import { describe, expect, it } from "vitest";
 import { hl7v2Jsonify } from "../src/processor";
 
 describe("hl7v2Jsonify plugin", () => {
   it("compiles single segment tree to JSON string of toJson output", async () => {
-    const msg =
-      "MSH|^~\\&|SENDER|FAC|RCVR|FAC|20250101010101||ADT^A01|MSG00001|P|2.5";
-    const file = await unified()
-      .use(hl7v2Parser)
-      .use(hl7v2Jsonify)
-      .process(msg);
+    const tree = m(
+      s(
+        "MSH",
+        f("|"),
+        f("^~\\&"),
+        f("SENDER"),
+        f("FAC"),
+        f("RCVR"),
+        f("FAC"),
+        f("20250101010101"),
+        f(""),
+        f(["ADT", "A01"]),
+        f("MSG00001"),
+        f("P"),
+        f("2.5")
+      )
+    );
+    const result = await unified().use(hl7v2Jsonify).stringify(tree);
 
-    expect(file.value).toEqual(msg);
-    expect(file.result).toEqual([
+    console.log(result);
+
+    expect(result).toEqual([
       {
         segment: "MSH",
         fields: [
@@ -35,16 +48,31 @@ describe("hl7v2Jsonify plugin", () => {
   });
 
   it("compiles multiple segment tree to JSON string of toJson output", async () => {
-    const msg = [
-      "MSH|^~\\&|SENDER|FAC|RCVR|FAC|20250101010101||ADT^A01|MSG00001|P|2.5",
-      "PID|1234567890",
-    ].join("\r");
-    const file = await unified()
-      .use(hl7v2Parser)
-      .use(hl7v2Jsonify)
-      .process(msg);
-    expect(file.value).toEqual(msg);
-    expect(file.result).toEqual([
+    // Given
+    const tree = m(
+      s(
+        "MSH",
+        f("|"),
+        f("^~\\&"),
+        f("SENDER"),
+        f("FAC"),
+        f("RCVR"),
+        f("FAC"),
+        f("20250101010101"),
+        f(""),
+        f(["ADT", "A01"]),
+        f("MSG00001"),
+        f("P"),
+        f("2.5")
+      ),
+      s("PID", f("1234567890"))
+    );
+
+    // WHEN
+    const result = await unified().use(hl7v2Jsonify).stringify(tree);
+
+    // THEN
+    expect(result).toEqual([
       {
         segment: "MSH",
         fields: [
