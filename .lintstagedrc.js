7005ea66ee78c573d23c57f82f55e937c6cb590f
Melek Somai <melek@rethinkhealth.io>
2025-11-24 13:55:33 -0500

    Adding support to lint-staged on commit

--- .lintstagedrc.js
+++
@@ -1,6 +0,0 @@
-export default {
-  // Run ultracite (biome) formatting on all staged files
-  "*": ["pnpm format"],
-  // Run syncpack when any package.json is modified (runs on all package.json files)
-  "**/package.json": () => ["pnpm syncpack:fix"],
-};
