2d7a5567c77ca252e2c94c565c98ef1d694121d1
Melek Somai <melek@rethinkhealth.io>
2026-03-03 17:30:51 -0500

    fix

--- packages/hl7v2-util-message-info/vitest.config.ts
+++ packages/hl7v2-util-message-info/vitest.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    globals: true,
     coverage: {
       provider: "v8",
       reporter: ["text", "json", "html"],
     },
+    globals: true,
   },
 });
