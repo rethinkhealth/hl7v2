4ad8ba1ff34c4ea5769219273a8016b7fb1beef5
Melek Somai <melek@rethinkhealth.io>
2026-05-07 17:28:52 -0700

    removed old examples

--- examples/glion-node/glion.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "@glion/cli/config";
-
-export default defineConfig({
-  entry: "./src/app.ts",
-  port: 2575,
-  // tls: { cert: "./certs/server.pem", key: "./certs/server.key" },
-  // watch: ["./src"],
-});
